7922e4cbb35da319b100f84126db7b58a9a9744c
Joseph John <joseph.john@anu.edu.au>
2026-06-11 23:57:52 +1000

    created ansible files for falco, kyverno, metric server, prometheus

--- infrastructure/ansible/8_metric_server/commands.sh
+++ infrastructure/ansible/8_metric_server/commands.sh
@@ -7,4 +7,4 @@ helm upgrade --install metrics-server metrics-server/metrics-server \
     --set args="{--secure-port=10251,--kubelet-insecure-tls}" \
     --set containerPort=10251
 
-ubectl top pods -n kube-system
+kubectl top pods -n kube-system
