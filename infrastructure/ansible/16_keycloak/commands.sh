a4a095ec20fecf5f63251ed717b914d7d0c6ccb3
Joseph John <joseph.john@anu.edu.au>
2026-06-17 20:52:45 +1000

    keycloak commands updated

--- infrastructure/ansible/16_keycloak/commands.sh
+++ infrastructure/ansible/16_keycloak/commands.sh
@@ -6,14 +6,17 @@ kubectl create -f keycloak-pool.yaml
 kubectl create -f keycloak-sc.yaml 
 
 
-kubectl patch storageclass keycloak-sc   -p\ 
-    '{"metadata":{"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+kubectl patch storageclass keycloak-sc -p '{"metadata":{"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm search repo bitnami
-helm install keycloak bitnami/keycloak
+helm install keycloak bitnami/keycloak --values values.yaml  -n keycloak
 
-kubectl patch storageclass keycloak-sc   -p\ 
-    '{"metadata":{"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
+
+kubectl patch svc keycloak -n keycloak -p '{"spec": {"type": "NodePort"}}'
+
+kubectl describe secret keycloak -n keycloak
+
+kubectl patch storageclass keycloak-sc -p '{"metadata":{"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
 
