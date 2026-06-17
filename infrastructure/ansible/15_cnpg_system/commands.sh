9a8c4ec19bcda31950e197c47d51b82aa6e02a20
Joseph John <joseph.john@anu.edu.au>
2026-06-17 19:49:01 +1000

    cnpg commands updated

--- infrastructure/ansible/15_cnpg_system/commands.sh
+++ infrastructure/ansible/15_cnpg_system/commands.sh
@@ -12,3 +12,39 @@ helm upgrade --install cnpg --namespace cnpg-system --create-namespace cnpg/clou
 
 helm upgrade --install cnpg --namespace cnpg-database --create-namespace --values values.yaml cnpg/cluster
 
+# Create database
+# -----------------
+
+postgres=# CREATE USER keycloak WITH PASSWORD 'keycloak-pwd';
+CREATE ROLE
+postgres=# CREATE DATABASE keycloak OWNER keycloak;
+CREATE DATABASE
+postgres=# GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
+GRANT
+postgres=# \c keycloak
+You are now connected to database "keycloak" as user "postgres".
+keycloak=# ALTER SCHEMA public OWNER TO keycloak;
+ALTER SCHEMA
+keycloak=# GRANT ALL ON SCHEMA public TO keycloak;
+GRANT
+keycloak=# ALTER DEFAULT PRIVILEGES IN SCHEMA public
+keycloak-# GRANT ALL ON TABLES TO keycloak;
+ALTER DEFAULT PRIVILEGES
+keycloak=# ALTER DEFAULT PRIVILEGES IN SCHEMA public
+keycloak-# GRANT ALL ON SEQUENCES TO keycloak;
+ALTER DEFAULT PRIVILEGES
+keycloak=# \dn+
+                          List of schemas
+  Name  |  Owner   |  Access privileges   |      Description       
+--------+----------+----------------------+------------------------
+ public | keycloak | keycloak=UC/keycloak+| standard public schema
+        |          | =U/keycloak          | 
+(1 row)
+
+
+
+kubectl get secret cnpg-cluster-superuser -n cnpg-database -o jsonpath="{.data.username}" | base64 --decode
+
+kubectl get secret cnpg-cluster-superuser -n cnpg-database -o jsonpath="{.data.password}" | base64 --decode
+    
+
