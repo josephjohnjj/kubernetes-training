8e0a85f4604013c8b38dd3f42e6ef5a37848dba9
Joseph John <joseph.john@anu.edu.au>
2026-06-17 20:54:10 +1000

    security group updated for keycloak

--- infrastructure/security_groups.tf
+++ infrastructure/security_groups.tf
@@ -164,6 +164,14 @@ resource "aws_security_group" "monitoring" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Keycloak port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
