9cd27c536ab5552a03235eeb331a02f8cac01ddd
Joseph John <joseph.john@anu.edu.au>
2026-07-28 11:30:58 +1000

    ingress controller using nginx

---
+++ infrastructure/ansible/20_ingress_controller/commands.sh
@@ -0,0 +1,7 @@
+  helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+
+  helm repo update
+
+  helm install ingress-nginx ingress-nginx/ingress-nginx   --namespace ingress-nginx   --create-namespace
+  
+  kubectl get svc -n ingress-nginx
