186f86275ecc85c2aecfe5d05b0c824d5d350729
Joseph John <joseph.john@anu.edu.au>
2026-08-07 16:36:28 +1000

    Ansible file for prometheus corrected.
    - File renamed.
    - Patch removed.

--- infrastructure/ansible/9_prometheus/commands.sh
+++ infrastructure/ansible/9_prometheus/commands.sh
@@ -1,7 +1,11 @@
+
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
 kubectl create namespace prometheus
+
+helm install prometheus prometheus-community/kube-prometheus-stack \
+  --namespace prometheus
 
 kubectl --namespace prometheus get pods -l "release=prometheus"
 
