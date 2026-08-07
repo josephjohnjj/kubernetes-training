87433fe8c703a4c6c732a94f6fc895d8a7e9c6fc
Joseph John <joseph.john@anu.edu.au>
2026-08-07 17:19:30 +1000

    Coomnd file file for installing Helm

---
+++ infrastructure/ansible/2_helm/commands.sh
@@ -0,0 +1,8 @@
+
+# Ona all control plane nodes, run the following commands to install Helm 3:
+
+wget https://get.helm.sh/helm-v3.19.0-linux-amd64.tar.gz
+
+tar -xvf helm-v3.19.0-linux-amd64.tar.gz 
+
+sudo cp linux-amd64/helm /usr/local/bin/helm
