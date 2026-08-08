840fa624ff2d4d8ca72d4a50864217faf6307c89
Joseph John <joseph.john@anu.edu.au>
2026-08-08 14:26:27 +1000

    New directory configure for all congi setup

---
+++ configuration/argocd.sh
@@ -0,0 +1,28 @@
+
+kubectl apply -f argocd-keycloak-secret.yaml
+
+kubectl get secret argocd-secret-keycloak -n argocd
+
+
+kubectl -n argocd patch secret argocd-secret \
+  --type merge \
+  -p '{"stringData":{"oidc.keycloak.clientSecret":"'"$(kubectl -n argocd get secret argocd-secret-keycloak -o jsonpath='{.data.clientSecret}' | base64 -d)"'"}}'
+
+kubectl edit configmap argocd-cm -n argocd
+
+# Under data add
+
+  oidc.config: |
+    name: Keycloak
+    issuer: http://keycloak.44.203.188.20.nip.io/realms/genome
+    clientID: argocd
+    enablePKCEAuthentication: true
+    requestedScopes:
+      - openid
+
+
+kubectl get configmap argocd-cm -n argocd -o yaml
+
+kubectl -n argocd rollout restart deployment argocd-server
+
+kubectl -n argocd rollout status deployment argocd-server
