af96ad84d5f6c694feb4df0fd1927032a07a9548
Joseph John <joseph.john@anu.edu.au>
2026-08-09 10:26:04 +1000

    removed jaeger ansible directory

---
+++ commands.md
@@ -0,0 +1,15 @@
+
+Jaeger
+------
+
+```bash
+helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
+
+helm install jaeger jaegertracing/jaeger -n jaeger --create-namespace
+```
+
+
+
+
+
+
--- infrastructure/ansible/14_jaeger/commands.sh
+++
@@ -1,8 +0,0 @@
-
-helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
-
-helm install jaeger jaegertracing/jaeger   -n observability   --create-namespace
-
-kubectl patch svc jaeger -n observability   -p '{"spec":{"type":"NodePort"}}'
-
-kubectl run curl --rm -it   --image=curlimages/curl   --restart=Never -- sh
