50caf2c410e900667c25e1021cbcb4ea29ac7a51
Joseph John <joseph.john@anu.edu.au>
2026-08-18 17:26:02 +1000

    switch documentation to sphinx book theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 project = "Kubernetes Training"
-# copyright = "2024, National Computational Infrastructure"
-author = "Name of the author"
+copyright = "2026, National Computational Infrastructure"
+author = "NCI Training"
 
 release = "0.1"
 version = "0.1.0"
@@ -17,11 +17,13 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
+    "sphinx_copybutton",
 ]
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3/", None),
     "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+    "pst": ("https://pydata-sphinx-theme.readthedocs.io/en/latest/", None),
 }
 intersphinx_disabled_domains = ["std"]
 
@@ -29,7 +31,34 @@ templates_path = ["_templates"]
 
 # -- Options for HTML output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
+html_theme_options = {
+    "path_to_docs": "docs/source",
+    "repository_url": "https://github.com/josephjohnjj/kubernetes-training",
+    "use_repository_button": True,
+    "home_page_in_toc": True,
+    "back_to_top_button": True,
+    "logo": {
+        "image_light": "_static/logo-light.png",
+        "image_dark": "_static/logo-dark.png",
+    },
+    "icon_links": [
+        {
+            "name": "NCI Documentation",
+            "url": "https://opus.nci.org.au/spaces/Help/pages/12583138/NCI+Help",
+            "icon": "fa-brands fa-confluence",
+            "type": "fontawesome",
+        },
+        {
+            "name": "Courses",
+            "url": "https://nci900-training-organisation.github.io/learning-resources/courses.html",
+            "icon": "fa-solid fa-graduation-cap",
+            "type": "fontawesome",
+        },
+    ],
+}
 
 # -- Options for EPUB output
 epub_show_urls = "footnote"
