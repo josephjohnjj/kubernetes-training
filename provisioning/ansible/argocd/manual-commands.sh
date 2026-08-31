9e05fa1140bcc8d0d336e6c684de44c3a5bb3486
Joseph John <joseph.john@anu.edu.au>
2026-08-31 22:33:26 +1000

    delete legacy directories

--- provisioning/ansible/argocd/manual-commands.sh
+++
@@ -1,6 +0,0 @@
-kubectl create namespace argocd
-
-kubectl patch storageclass mgmnt-sc \
-  -p '{"metadata":{"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
-
-kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
--- provisioning/ansible/haproxy/manual-commands.sh
+++
@@ -1,7 +0,0 @@
-
-sudo cp /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.bak
-sudo vim /etc/haproxy/haproxy.cfg
-
-
-sudo systemctl reload haproxy
-sudo ss -tulnp | grep 5601
